0ae399c3dd0dbac1c27915f71d666357a72fc17c
John Benedict Listangco <jbslistangco2024@plm.edu.ph>
2025-04-17 19:00:22 +0800

    Edit

--- core/apps.py
+++ core/apps.py
@@ -5,7 +5,3 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
-<<<<<<< HEAD
-=======
-class CoreConfig()
->>>>>>> ce10fcaec10b37fb10d82123ef3c9e99307918c4

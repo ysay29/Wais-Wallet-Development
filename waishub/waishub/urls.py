f51d67d42e4d24c010401b29c760fe8914484fe9
John Benedict Listangco <jbslistangco2024@plm.edu.ph>
2025-04-26 23:09:29 +0800

    Edit

--- waishub/waishub/urls.py
+++ waishub/waishub/urls.py
@@ -20,6 +20,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('members/', include('django.contrib.auth.urls')),
-    path('members/', include('members.urls')),
 ]

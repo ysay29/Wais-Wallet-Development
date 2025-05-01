15fec04b804f1d2fadfb79b1216c1054440ad254
John Benedict Listangco <jbslistangco2024@plm.edu.ph>
2025-05-01 22:02:30 +0800

    Edit

--- waishub/waishub/settings.py
+++ waishub/waishub/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'core',
     'authentication',
     'dashboard',
-    'Transaction',
+    'transaction',
 ]
 
 MIDDLEWARE = [

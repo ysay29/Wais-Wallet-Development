15e1fb07729f93a168e9cfe5a8ae00382ebfd1cf
cessamaeeee <princessmae.sanchez04@gmail.com>
2025-05-09 06:25:57 +0800

    .

--- waishub/Transaction/urls.py
+++ waishub/Transaction/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import add_transaction, transactions_list
 
 urlpatterns = [
-    path('', add_transaction, name='add_transaction'),
-    path('', transactions_list, name='transactions_list'),
+    path('add/', add_transaction, name='add_transaction'),
+    path('list/', transactions_list, name='transactions_list'),
 ]

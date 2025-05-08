acedf89b657b2601f1c8e8de8b086aecec755977
cessamaeeee <princessmae.sanchez04@gmail.com>
2025-05-08 21:47:15 +0800

    HAHA

--- waishub/TransactionsList/urls.py
+++ waishub/TransactionsList/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import transactions_view
+from .views import transactions_view, add_transaction
 
 urlpatterns = [
     path('', transactions_view, name='transactions'),
+    path('add/', add_transaction, name='add_transaction'),
 ]
--- waishub/TransactionsList/views.py
+++ waishub/TransactionsList/views.py
@@ -2,21 +2,30 @@ from django.shortcuts import render, redirect
 from .models import Transaction
 from django.utils import timezone
 
-def transactions_view(request):
-    current_month = timezone.now().strftime('%B %Y')
-
+def add_transaction(request):
     if request.method == 'POST':
-        t   = request.POST.get('type')
+        t = request.POST.get('type')
         cat = request.POST.get('category')
         amt = request.POST.get('amount')
-        dt  = request.POST.get('date')
+        dt = request.POST.get('date')
+        month = timezone.datetime.strptime(dt, "%Y-%m-%d").strftime('%B %Y')
 
-        Transaction.objects.create(type=t, category=cat, amount=amt, date=dt, month=current_month)
+        Transaction.objects.create(
+            type=t,
+            category=cat,
+            amount=amt,
+            date=dt,
+            month=month
+        )
         return redirect('transactions')
+    return render(request, 'TransactionsList/add.html')
 
-    transactions = Transaction.objects.filter(month=current_month)
 
-    return render(request, 'transactions.html', {
+def transactions_view(request):
+    current_month = timezone.now().strftime('%B %Y')
+    transactions = Transaction.objects.filter(month=current_month).order_by('-date')
+
+    return render(request, 'TransactionsList/transactions.html', {
         'transactions': transactions,
         'current_month': current_month
     })

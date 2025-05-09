47801ea30249dd48e1d0ff50b7b4c3c9d89e7bfb
chezca-v <franchezca.z.bnyd@gmail.com>
2025-05-09 16:27:21 +0800

    reminder

--- waishub/dashboard/apps.py
+++ waishub/dashboard/apps.py
@@ -4,3 +4,20 @@ from django.apps import AppConfig
 class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
+
+class DashboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dashboard'
+
+    def ready(self):
+        from apscheduler.schedulers.background import BackgroundScheduler
+        from dashboard.tasks import send_transaction_reminders
+        import atexit
+
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(send_transaction_reminders, 'cron', minute='*')  # Every minute
+        scheduler.start()
+
+        # Stop scheduler on shutdown
+        atexit.register(lambda: scheduler.shutdown(wait=False))
+

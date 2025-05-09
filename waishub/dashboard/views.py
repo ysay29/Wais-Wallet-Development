b9b0a0dbeb49d1b4ad1cb40582ccc60f3c08e669
chezca-v <franchezca.z.bnyd@gmail.com>
2025-05-09 11:03:31 +0800

    no login back after logout

--- waishub/dashboard/views.py
+++ waishub/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache, cache_control
 from django.contrib.auth import logout
 
 #@login_required
@@ -9,3 +10,9 @@ def index(request):
 def logout_user(request):
     logout(request)
     return redirect('login') 
+
+@never_cache
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')

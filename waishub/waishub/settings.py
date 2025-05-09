4f2a572ecd3d3824cf0d5b893541c75df7c062cb
chezca-v <franchezca.z.bnyd@gmail.com>
2025-05-09 19:09:08 +0800

    timezone ahhh

--- waishub/waishub/settings.py
+++ waishub/waishub/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'authentication',
     'Transaction',
     'savings',
-    'dashboard.apps.DashboardConfig'
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila' 
 
 USE_I18N = True
 

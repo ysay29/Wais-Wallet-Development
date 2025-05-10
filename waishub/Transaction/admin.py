89542f6e6a337578c0973ae1b2b9bedfb71182d6
jblistangco2024 <jbslistagco2024@plm.edu.ph>
2025-05-11 00:27:27 +0800

    .

--- waishub/Transaction/admin.py
+++ waishub/Transaction/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Transaction, UserCategory
+from savings.models import Saving
 
 admin.site.register(Transaction)
 admin.site.register(UserCategory)
+admin.site.register(Saving)
--- waishub/savings/models.py
+++ waishub/savings/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Saving(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     date = models.DateField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100, blank=True)

2ae41f728aedd1cc3bb1382d4048abcfca80ac29
John Benedict Listangco <jbslistangco2024@plm.edu.ph>
2025-05-11 19:50:29 +0800

    Edit landing page routing

--- waishub/dashboard/urls.py
+++ waishub/dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, logout_user, notifications_view, settings_view, mark_notification_read, add_expense, filter_dashboard_data, update_username, delete_all_data
 
 urlpatterns = [
-    path('', index, name='dashboard'),  # dashboard/index
+    path('dashboard/', index, name='dashboard'),  # dashboard/index
     path('logout/', logout_user, name='logout'), #logout from dashboard
     path('notifications/', notifications_view, name='notifications'), #notifications page
     path('settings/', settings_view, name='settings'), #settings page

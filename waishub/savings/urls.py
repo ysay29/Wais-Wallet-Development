dc0e73aea35b9021220ad26c5b96dd6cfef9dca1
chezca-v <franchezca.z.bnyd@gmail.com>
2025-05-11 17:32:19 +0800

    ,

--- waishub/savings/urls.py
+++ waishub/savings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import savings_summary, add_savings, budget_view, delete_goal
 
 urlpatterns = [
-    path('', savings_summary, name='savings'),
+    path('savings/', savings_summary, name='savings'),
     path('add-savings/', add_savings, name='add_savings_goal'),
     path('budgets/', budget_view, name='budget'),
     path('delete-goal/<int:id>/', delete_goal, name='delete_goal'),
